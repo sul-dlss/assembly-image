c04219f94fb38ed0426e674057caaf76af24d58c
Peter Mangiafico <peter@mangiafico.org>
2012-06-08 12:16:01 -0700

    added a new method to the Assembly::Images class to allow for batch adding of color profiles to all tiffs in a directory; allow batch methods to run recursively

--- lib/assembly-image/images.rb
+++ lib/assembly-image/images.rb
@@ -2,6 +2,50 @@ module Assembly
   
   # The Images class contains methods to operate on multiple images in batch.
   class Images
+
+    # Pass in a source path and have exif color profile descriptions added to all images contained.
+    # This is useful if your source TIFFs do not have color profile descriptions in the EXIF data, but you know what it should be.
+    # This will allow the images to pass the validty check and have JP2s created successfully.
+    #
+    # Note you will need full read/write access to the source path so that new EXIF data can be saved.
+    #
+    # @param [String] path full path to the directory containing TIFFs
+    # @param [String] profile name to be added, current options are 'Adobe RBG 1998','Dot Gain 20%','sRGB IEC61966-2.1'
+    #
+    # @param [Hash] params Optional parameters specified as a hash, using symbols for options:
+    #   * :force => if set to true, force overwrite a color profile description even if it already exists (default: false)
+    #   * :recusrive => if set to true, directories will be searched recursively for TIFFs from the source specified, false searches the top level only (default: false)
+    #
+    # Example:
+    #  Assembly::Images.batch_add_exif_profile_description('/full_path_to_tifs','Adobe RGB 1998')    
+    def self.batch_add_exif_profile_description(source,profile_name,params={})
+
+      recursive = params[:recursive] || false
+      force = params[:force] || false
+      
+      raise "Input path does not exist" unless File.directory?(source)
+      
+      puts "Source: #{source}"
+
+      # iterate over input directory looking for tifs
+      pattern = recursive ? '**/*.tif' : '*.tif*' 
+      Dir.glob(File.join(source,pattern)).each do |tiff_file|
+        exif=MiniExiftool.new tiff_file
+        begin
+          if exif.profiledescription == nil || force
+            input_profile = profile_name.gsub(/[^[:alnum:]]/, '')   # remove all non alpha-numeric characters, so we can get to a filename
+            path_to_profiles    = File.join(Assembly::PATH_TO_IMAGE_GEM,'profiles')
+            input_profile_file = File.join(path_to_profiles,"#{input_profile}.icc")
+            command="exiftool '-icc_profile<=#{input_profile_file}' #{tiff_file}"
+            result=`#{command}`
+            raise "profile addition command failed: #{command} with result #{result}" unless $?.success?
+          end
+        rescue Exception => e
+          puts "** Error for #{File.basename(tiff_file)}: #{e.message}"
+        end
+      end
+      return 'Complete'
+    end
     
     # Pass in a source path and get JP2s generate for each tiff that is in the source path
     #
@@ -13,7 +57,8 @@ module Assembly
     # @param [Hash] params Optional parameters specified as a hash, using symbols for options:
     #   * :output=>'/full/path_to_jp2' # specifies full path to folder where jp2s will be created (default: jp2 subdirectory from source path)
     #   * :overwrite => if set to false, an existing JP2 file with the same name won't be overwritten (default: false)
-    #  
+    #   * :recusrive => if set to true, directories will be searched recursively for TIFFs from the source specified, false searches the top level only (default: false)
+    #
     # Example:
     #  Assembly::Images.batch_generate_jp2('/full_path_to_tifs')
     def self.batch_generate_jp2(source,params={})
@@ -21,15 +66,18 @@ module Assembly
         raise "Input path does not exist" unless File.directory?(source)
         output = params[:output] || File.join(source,'jp2') # default output directgory is jp2 sub-directory from source
         overwrite = params[:overwrite] || false
-        
+        recursive = params[:recursive] || false
+                
         Dir.mkdir(output) unless File.directory?(output) # attemp to make output directory
         raise "Output path does not exist or could not be created" unless File.directory?(output) 
 
         puts "Source: #{source}"
         puts "Destination: #{output}"
   
+        pattern = recursive ? '**/*.tif' : '*.tif*' 
+
         # iterate over input directory looking for tifs
-        Dir.glob(File.join(source,"*.tif*")).each do |tiff_file|
+        Dir.glob(File.join(source,pattern)).each do |tiff_file|
           source_img=Assembly::Image.new(tiff_file)
           output_img=File.join(output,File.basename(tiff_file,File.extname(tiff_file))+'.jp2') # output image gets same file name as source, but with a jp2 extension and in the correct output directory
           begin
--- spec/images_spec.rb
+++ spec/images_spec.rb
@@ -8,12 +8,34 @@ describe Assembly::Images do
     lambda{Assembly::Images.batch_generate_jp2('/junk/path')}.should raise_error
   end
 
-  it "should run and produe jp2s from input tiffs" do
+  it "should run and batch produe jp2s from input tiffs" do
     ['test1','test2','test3'].each {|image| generate_test_image(File.join(TEST_INPUT_DIR,"#{image}.tif")) }
     Assembly::Images.batch_generate_jp2(TEST_INPUT_DIR,:output=>TEST_OUTPUT_DIR)
     File.directory?(TEST_OUTPUT_DIR).should be true
     ['test1','test2','test3'].each {|image| is_jp2?(File.join(TEST_OUTPUT_DIR,"#{image}.jp2")).should be true }    
   end
+
+  it "should run and batch add color profile descriptions input tiffs with no color profile descriptions" do
+    ['test1','test2','test3'].each {|image| generate_test_image(File.join(TEST_INPUT_DIR,"#{image}.tif"),:profile=>'') }
+    ['test1','test2','test3'].each {|image| Assembly::Image.new(File.join(TEST_INPUT_DIR,"#{image}.tif")).exif.profiledescription.should be nil}    
+    Assembly::Images.batch_add_exif_profile_description(TEST_INPUT_DIR,'Adobe RGB 1998')
+    ['test1','test2','test3'].each {|image| Assembly::Image.new(File.join(TEST_INPUT_DIR,"#{image}.tif")).exif.profiledescription.should == 'Adobe RGB (1998)'}    
+  end
+  
+  it "should run and batch add color profile descriptions input tiffs, forcing over existing color profile descriptions" do
+    ['test1','test2','test3'].each {|image| generate_test_image(File.join(TEST_INPUT_DIR,"#{image}.tif")) }
+    ['test1','test2','test3'].each {|image| Assembly::Image.new(File.join(TEST_INPUT_DIR,"#{image}.tif")).exif.profiledescription.should == 'sRGB IEC61966-2.1'}    
+    Assembly::Images.batch_add_exif_profile_description(TEST_INPUT_DIR,'Adobe RGB 1998',:force=>true) # force overwrite
+    ['test1','test2','test3'].each {|image| Assembly::Image.new(File.join(TEST_INPUT_DIR,"#{image}.tif")).exif.profiledescription.should == 'Adobe RGB (1998)'}    
+  end
+
+  it "should run and batch add color profile descriptions input tiffs, not overwriting existing color profile descriptions" do
+    ['test1','test2','test3'].each {|image| generate_test_image(File.join(TEST_INPUT_DIR,"#{image}.tif")) }
+    ['test1','test2','test3'].each {|image| Assembly::Image.new(File.join(TEST_INPUT_DIR,"#{image}.tif")).exif.profiledescription.should == 'sRGB IEC61966-2.1'}    
+    Assembly::Images.batch_add_exif_profile_description(TEST_INPUT_DIR,'Adobe RGB 1998') # do not force overwrite
+    ['test1','test2','test3'].each {|image| Assembly::Image.new(File.join(TEST_INPUT_DIR,"#{image}.tif")).exif.profiledescription.should == 'sRGB IEC61966-2.1'}    
+  end
+
 
   after(:each) do
     # after each test, empty out the input and output test directories
