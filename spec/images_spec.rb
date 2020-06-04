45023f60b75fa644b041ca62f7f75c7c5d036b77
Justin Coyne <jcoyne@justincoyne.com>
2020-06-04 12:22:50 -0500

    Add context blocks for each test. Use let rather than ivars

--- spec/images_spec.rb
+++ spec/images_spec.rb
@@ -4,43 +4,43 @@ require 'spec_helper'
 
 # rubocop:disable Metrics/BlockLength
 RSpec.describe Assembly::Images do
-  it 'should not run if no input folder is passed in' do
+  it 'does not run if no input folder is passed in' do
     expect{ Assembly::Images.batch_generate_jp2('') }.to raise_error
   end
 
-  it 'should not run if a non-existent input folder is passed in' do
+  it 'does not run if a non-existent input folder is passed in' do
     expect{ Assembly::Images.batch_generate_jp2('/junk/path') }.to raise_error
   end
 
-  it 'should run and batch produe jp2s from input tiffs' do
+  it 'runs and batch produces jp2s from input tiffs' do
     ['test1', 'test2', 'test3'].each { |image| generate_test_image(File.join(TEST_INPUT_DIR, "#{image}.tif")) }
     Assembly::Images.batch_generate_jp2(TEST_INPUT_DIR, output: TEST_OUTPUT_DIR)
     expect(File.directory?(TEST_OUTPUT_DIR)).to be true
     ['test1', 'test2', 'test3'].each { |image| expect(File.join(TEST_OUTPUT_DIR, "#{image}.jp2")).to be_a_jp2 }
   end
 
-  it 'should run and batch add color profile descriptions input tiffs with no color profile descriptions' do
+  it 'runs and batch add color profile descriptions input tiffs with no color profile descriptions' do
     ['test1', 'test2', 'test3'].each { |image| generate_test_image(File.join(TEST_INPUT_DIR, "#{image}.tif"), profile: '') }
     ['test1', 'test2', 'test3'].each { |image| expect(Assembly::Image.new(File.join(TEST_INPUT_DIR, "#{image}.tif")).exif.profiledescription).to be nil }
     Assembly::Images.batch_add_exif_profile_descr(TEST_INPUT_DIR, 'Adobe RGB 1998')
     ['test1', 'test2', 'test3'].each { |image| expect(Assembly::Image.new(File.join(TEST_INPUT_DIR, "#{image}.tif")).exif.profiledescription).to eq 'Adobe RGB (1998)' }
   end
 
-  it 'should run and batch add color profile descriptions input tiffs, forcing over existing color profile descriptions' do
+  it 'runs and batch add color profile descriptions input tiffs, forcing over existing color profile descriptions' do
     ['test1', 'test2', 'test3'].each { |image| generate_test_image(File.join(TEST_INPUT_DIR, "#{image}.tif")) }
     ['test1', 'test2', 'test3'].each { |image| expect(Assembly::Image.new(File.join(TEST_INPUT_DIR, "#{image}.tif")).exif.profiledescription).to eq 'sRGB IEC61966-2.1' }
     Assembly::Images.batch_add_exif_profile_descr(TEST_INPUT_DIR, 'Adobe RGB 1998', force: true) # force overwrite
     ['test1', 'test2', 'test3'].each { |image| expect(Assembly::Image.new(File.join(TEST_INPUT_DIR, "#{image}.tif")).exif.profiledescription).to eq 'Adobe RGB (1998)' }
   end
 
-  it 'should run and batch add color profile descriptions input tiffs, not overwriting existing color profile descriptions' do
+  it 'runs and batch add color profile descriptions input tiffs, not overwriting existing color profile descriptions' do
     ['test1', 'test2', 'test3'].each { |image| generate_test_image(File.join(TEST_INPUT_DIR, "#{image}.tif")) }
     ['test1', 'test2', 'test3'].each { |image| expect(Assembly::Image.new(File.join(TEST_INPUT_DIR, "#{image}.tif")).exif.profiledescription).to eq 'sRGB IEC61966-2.1' }
     Assembly::Images.batch_add_exif_profile_descr(TEST_INPUT_DIR, 'Adobe RGB 1998') # do not force overwrite
     ['test1', 'test2', 'test3'].each { |image| expect(Assembly::Image.new(File.join(TEST_INPUT_DIR, "#{image}.tif")).exif.profiledescription).to eq 'sRGB IEC61966-2.1' }
   end
 
-  after(:each) do
+  after do
     # after each test, empty out the input and output test directories
     remove_files(TEST_INPUT_DIR)
     remove_files(TEST_OUTPUT_DIR)
