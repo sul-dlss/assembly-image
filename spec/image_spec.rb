aa07bb2c3556e15b3a1835da6b50e84e9f288449
Peter Mangiafico <peter@mangiafico.org>
2022-06-17 16:12:06 -0700

    refactor tests

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -6,12 +6,13 @@ require 'fileutils'
 RSpec.describe Assembly::Image do
   let(:assembly_image) { described_class.new(input_path) }
   let(:input_path) { TEST_TIF_INPUT_FILE }
+  let(:output_file) { File.join(TEST_OUTPUT_DIR, File.basename(input_path).gsub('.tif', '.jp2')) }
 
   before { cleanup }
 
   describe '#jp2_filename' do
     it 'indicates the default jp2 filename' do
-      expect(assembly_image.jp2_filename).to eq TEST_TIF_INPUT_FILE.gsub('.tif', '.jp2')
+      expect(assembly_image.jp2_filename).to eq input_path.gsub('.tif', '.jp2')
     end
 
     context 'with a file with no extension' do
@@ -33,197 +34,203 @@ RSpec.describe Assembly::Image do
     end
 
     context 'when given an uncompressed compressed RGB tif with more than 4GB of image data', skip: 'This test will create a 4GB test image and a 4GB temporary image, so skipping by default.' do
+      let(:input_path) { File.join(TEST_INPUT_DIR, 'rgb.tif') }
+
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE, compress: 'none', width: '37838', height: '37838')
+        generate_test_image(input_path, compress: 'none', width: '37838', height: '37838')
       end
 
       it 'creates the jp2 with a temp file' do
-        expect(File).to exist TEST_TIF_INPUT_FILE
-        expect(File).not_to exist TEST_JP2_OUTPUT_FILE
-        result = assembly_image.create_jp2(output: TEST_JP2_OUTPUT_FILE)
+        expect(File).to exist input_path
+        expect(File).not_to exist output_file
+        result = assembly_image.create_jp2(output: output_file)
         expect(assembly_image.tmp_path).not_to be_nil
         expect(result).to be_a_kind_of described_class
-        expect(result.path).to eq TEST_JP2_OUTPUT_FILE
-        expect(TEST_JP2_OUTPUT_FILE).to be_a_jp2
+        expect(result.path).to eq output_file
+        expect(output_file).to be_a_jp2
         expect(result.exif.colorspace).to eq 'sRGB'
-        jp2 = described_class.new(TEST_JP2_OUTPUT_FILE)
+        jp2 = described_class.new(output_file)
         expect(jp2.height).to eq 37_838
         expect(jp2.width).to eq 37_838
       end
     end
 
     context 'when given an LZW compressed RGB tif with more than 4GB of image data', skip: 'This test will create a 4GB temporary image, so skipping by default.' do
+      let(:input_path) { File.join(TEST_INPUT_DIR, 'lzw.tif') }
+
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE, compress: 'lzw', width: '37838', height: '37838')
+        generate_test_image(input_path, compress: 'lzw', width: '37838', height: '37838')
       end
 
       it 'creates the jp2 with a temp file' do
-        expect(File).to exist TEST_TIF_INPUT_FILE
-        expect(File).not_to exist TEST_JP2_OUTPUT_FILE
+        expect(File).to exist input_path
+        expect(File).not_to exist output_file
         expect(assembly_image.exif.samplesperpixel).to be 3
         expect(assembly_image.exif.bitspersample).to eql '8 8 8'
         expect(assembly_image).to be_a_valid_image
         expect(assembly_image).to be_jp2abl
-        result = assembly_image.create_jp2(output: TEST_JP2_OUTPUT_FILE)
+        result = assembly_image.create_jp2(output: output_file)
         expect(assembly_image.tmp_path).not_to be_nil
         expect(result).to be_a_kind_of described_class
-        expect(result.path).to eq TEST_JP2_OUTPUT_FILE
-        expect(TEST_JP2_OUTPUT_FILE).to be_a_jp2
+        expect(result.path).to eq output_file
+        expect(output_file).to be_a_jp2
         expect(result.exif.colorspace).to eq 'sRGB'
-        jp2 = described_class.new(TEST_JP2_OUTPUT_FILE)
+        jp2 = described_class.new(output_file)
         expect(jp2.height).to eq 37_838
         expect(jp2.width).to eq 37_838
       end
     end
 
     context 'when given a bitonal tif' do
-      let(:input_path) { "#{TEST_TIF_INPUT_FILE}_bitonal.tif" }
-      let(:test_jp2_output_file) { "#{TEST_JP2_OUTPUT_FILE}_bitonal.jp2" }
+      let(:input_path) { File.join(TEST_INPUT_DIR, 'bitonal.tif') }
 
       before do
-        File.delete(input_path) if File.exist? input_path
-        File.delete(test_jp2_output_file) if File.exist? test_jp2_output_file
         generate_test_image(input_path, color: 'bin', bands: 1, depth: 1)
       end
 
       it 'creates valid jp2' do
         expect(File).to exist input_path
-        expect(File).not_to exist test_jp2_output_file
+        expect(File).not_to exist output_file
         expect(assembly_image.exif.samplesperpixel).to be 1
         expect(assembly_image.exif.bitspersample).to be 1
         expect(assembly_image).not_to have_color_profile
-        result = assembly_image.create_jp2(output: test_jp2_output_file)
+        result = assembly_image.create_jp2(output: output_file)
         expect(result).to be_a_kind_of described_class
-        expect(result.path).to eq test_jp2_output_file
-        expect(test_jp2_output_file).to be_a_jp2
+        expect(result.path).to eq output_file
+        expect(output_file).to be_a_jp2
         expect(result.exif.colorspace).to eq 'Grayscale'
       end
     end
 
     context 'when given a color tif but bitonal image data (1 channels and 1 bits per pixel)' do
+      let(:input_path) { File.join(TEST_INPUT_DIR, 'color.tif') }
+
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE, color: 'bin', bands: 3)
+        generate_test_image(input_path, color: 'bin', bands: 3)
       end
 
       it 'creates color jp2' do
-        expect(File).to exist TEST_TIF_INPUT_FILE
-        expect(File).not_to exist TEST_JP2_OUTPUT_FILE
+        expect(File).to exist input_path
+        expect(File).not_to exist output_file
         expect(assembly_image).not_to have_color_profile
         expect(assembly_image.exif.samplesperpixel).to be 3
         expect(assembly_image.exif.bitspersample).to eql '8 8 8'
         expect(assembly_image).to be_a_valid_image
         expect(assembly_image).to be_jp2able
-        result = assembly_image.create_jp2(output: TEST_JP2_OUTPUT_FILE)
+        result = assembly_image.create_jp2(output: output_file)
         expect(result).to be_a_kind_of described_class
-        expect(result.path).to eq TEST_JP2_OUTPUT_FILE
-        expect(TEST_JP2_OUTPUT_FILE).to be_a_jp2
+        expect(result.path).to eq output_file
+        expect(output_file).to be_a_jp2
         expect(result.exif.colorspace).to eq 'sRGB'
       end
     end
 
     context 'when given a graycale tif but with bitonal image data (1 channel and 1 bits per pixel)' do
-      let(:input_path) { "#{TEST_TIF_INPUT_FILE}_gray.tif" }
-      let(:test_jp2_output_file) { "#{TEST_JP2_OUTPUT_FILE}_gray.jp2" }
+      let(:input_path) { File.join(TEST_INPUT_DIR, 'gray.tif') }
 
       before do
-        File.delete(input_path) if File.exist? input_path
-        File.delete(test_jp2_output_file) if File.exist? test_jp2_output_file
         generate_test_image(input_path, color: 'grey', bands: 1)
       end
 
       it 'creates grayscale jp2' do
         expect(File).to exist input_path
-        expect(File).not_to exist test_jp2_output_file
+        expect(File).not_to exist output_file
         expect(assembly_image).not_to have_color_profile
         expect(assembly_image.exif.samplesperpixel).to be 1
         expect(assembly_image.exif.bitspersample).to be 8
         expect(assembly_image).to be_a_valid_image
         expect(assembly_image).to be_jp2able
-        result = assembly_image.create_jp2(output: test_jp2_output_file)
-        expect(test_jp2_output_file).to be_a_jp2
+        result = assembly_image.create_jp2(output: output_file)
+        expect(output_file).to be_a_jp2
         expect(result).to be_a_kind_of described_class
-        expect(result.path).to eq test_jp2_output_file
+        expect(result.path).to eq output_file
         expect(result.exif.colorspace).to eq 'Grayscale'
       end
     end
 
     context 'when given a color tif but with greyscale image data (1 channel and 8 bits per pixel)' do
+      let(:input_path) { File.join(TEST_INPUT_DIR, 'color_gray.tif') }
+
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE, color: 'grey')
+        generate_test_image(input_path, color: 'grey')
       end
 
       it 'creates color jp2' do
-        expect(File).to exist TEST_TIF_INPUT_FILE
-        expect(File).not_to exist TEST_JP2_OUTPUT_FILE
+        expect(File).to exist input_path
+        expect(File).not_to exist output_file
         expect(assembly_image.exif.samplesperpixel).to be 3
         expect(assembly_image.exif.bitspersample).to eql '8 8 8'
         expect(assembly_image).to be_a_valid_image
         expect(assembly_image).to be_jp2able
         expect(assembly_image).not_to have_color_profile
-        result = assembly_image.create_jp2(output: TEST_JP2_OUTPUT_FILE)
+        result = assembly_image.create_jp2(output: output_file)
         expect(result).to be_a_kind_of described_class
-        expect(result.path).to eq TEST_JP2_OUTPUT_FILE
-        expect(TEST_JP2_OUTPUT_FILE).to be_a_jp2
+        expect(result.path).to eq output_file
+        expect(output_file).to be_a_jp2
         expect(result.exif.colorspace).to eq 'sRGB'
       end
     end
 
     context 'when given a cmyk tif' do
+      let(:input_path) { File.join(TEST_INPUT_DIR, 'cmky.tif') }
+
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE, color: 'cmyk', cg_type: 'cmyk', profile: 'cmyk', bands: 4)
+        generate_test_image(input_path, color: 'cmyk', cg_type: 'cmyk', profile: 'cmyk', bands: 4)
       end
 
       it 'creates jp2' do
-        expect(File).to exist TEST_TIF_INPUT_FILE
-        expect(File).not_to exist TEST_JP2_OUTPUT_FILE
+        expect(File).to exist input_path
+        expect(File).not_to exist output_file
         expect(assembly_image.exif.samplesperpixel).to be 4
         expect(assembly_image.exif.bitspersample).to eql '8 8 8 8'
         expect(assembly_image).to be_a_valid_image
         expect(assembly_image).to be_jp2able
         expect(assembly_image).to have_color_profile
-        result = assembly_image.create_jp2(output: TEST_JP2_OUTPUT_FILE)
+        result = assembly_image.create_jp2(output: output_file)
         expect(result).to be_a_kind_of described_class
-        expect(result.path).to eq TEST_JP2_OUTPUT_FILE
-        expect(TEST_JP2_OUTPUT_FILE).to be_a_jp2
+        expect(result.path).to eq output_file
+        expect(output_file).to be_a_jp2
         # TODO: figure out why result.exif.colorspace is nil
         # expect(result.exif.colorspace).to eq 'sRGB'
       end
     end
 
     context 'when the source image has no profile' do
+      let(:input_path) { File.join(TEST_INPUT_DIR, 'no_profile.tif') }
+
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE)
+        generate_test_image(input_path)
       end
 
       it 'creates a jp2' do
-        expect(File).to exist TEST_TIF_INPUT_FILE
-        expect(File).not_to exist TEST_JP2_OUTPUT_FILE
+        expect(File).to exist input_path
+        expect(File).not_to exist output_file
         expect(assembly_image.exif.samplesperpixel).to be 3
         expect(assembly_image.exif.bitspersample).to eql '8 8 8'
         expect(assembly_image).not_to have_color_profile
         expect(assembly_image).to be_a_valid_image
         expect(assembly_image).to be_jp2able
-        assembly_image.create_jp2(output: TEST_JP2_OUTPUT_FILE)
-        expect(TEST_JP2_OUTPUT_FILE).to be_a_jp2
+        assembly_image.create_jp2(output: output_file)
+        expect(output_file).to be_a_jp2
       end
     end
 
     context "when the output file exists and you don't allow overwriting" do
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE)
-        FileUtils.touch(TEST_JP2_OUTPUT_FILE)
+        generate_test_image(input_path)
+        FileUtils.touch(output_file) # just need a file with this name, don't care what
       end
 
       it 'does not run' do
-        expect(File).to exist TEST_TIF_INPUT_FILE
-        expect(File).to exist TEST_JP2_OUTPUT_FILE
-        expect { assembly_image.create_jp2(output: TEST_JP2_OUTPUT_FILE) }.to raise_error(SecurityError)
+        expect(File).to exist input_path
+        expect(File).to exist output_file
+        expect { assembly_image.create_jp2(output: output_file) }.to raise_error(SecurityError)
       end
     end
 
     context 'when given a test tiff' do
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE)
+        generate_test_image(input_path)
       end
 
       it 'gets the correct image height and width' do
@@ -234,18 +241,18 @@ RSpec.describe Assembly::Image do
 
     context 'when the input file is a jp2' do
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE)
+        generate_test_image(input_path)
       end
 
       it 'does not run' do
-        expect(File).to exist TEST_TIF_INPUT_FILE
-        expect(File).not_to exist TEST_JP2_OUTPUT_FILE
+        expect(File).to exist input_path
+        expect(File).not_to exist output_file
         expect(assembly_image).not_to have_color_profile
         expect(assembly_image).to be_a_valid_image
         expect(assembly_image).to be_jp2able
-        assembly_image.create_jp2(output: TEST_JP2_OUTPUT_FILE)
-        expect(TEST_JP2_OUTPUT_FILE).to be_a_jp2
-        jp2_file = described_class.new(TEST_JP2_OUTPUT_FILE)
+        assembly_image.create_jp2(output: output_file)
+        expect(output_file).to be_a_jp2
+        jp2_file = described_class.new(output_file)
         expect(jp2_file).to be_valid_image
         expect(jp2_file).not_to be_jp2able
         expect { jp2_file.create_jp2 }.to raise_error(RuntimeError)
@@ -254,11 +261,11 @@ RSpec.describe Assembly::Image do
 
     context 'when you specify a bogus output profile' do
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE)
+        generate_test_image(input_path)
       end
 
       it 'runs, because this is not currently an option' do
-        expect(File).to exist TEST_TIF_INPUT_FILE
+        expect(File).to exist input_path
         result = assembly_image.create_jp2(output_profile: 'bogusness')
         expect(result).to be_a_kind_of described_class
         expect(result.path).to eq TEST_JP2_INPUT_FILE
@@ -284,11 +291,11 @@ RSpec.describe Assembly::Image do
 
     context 'when no output file is specified' do
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE)
+        generate_test_image(input_path)
       end
 
       it 'creates a jp2 of the same filename and in the same location as the input and cleans up the tmp file' do
-        expect(File).to exist TEST_TIF_INPUT_FILE
+        expect(File).to exist input_path
         expect(File.exist?(TEST_JP2_INPUT_FILE)).to be false
         result = assembly_image.create_jp2
         expect(result).to be_a_kind_of described_class
@@ -300,17 +307,17 @@ RSpec.describe Assembly::Image do
 
     context 'when the output file exists and you allow overwriting' do
       before do
-        generate_test_image(TEST_TIF_INPUT_FILE)
-        FileUtils.touch(TEST_JP2_OUTPUT_FILE)
+        generate_test_image(input_path)
+        FileUtils.touch(output_file) # just need a file with this name, don't care what
       end
 
       it 'recreates jp2' do
-        expect(File).to exist TEST_TIF_INPUT_FILE
-        expect(File).to exist TEST_JP2_OUTPUT_FILE
-        result = assembly_image.create_jp2(output: TEST_JP2_OUTPUT_FILE, overwrite: true)
+        expect(File).to exist input_path
+        expect(File).to exist output_file
+        result = assembly_image.create_jp2(output: output_file, overwrite: true)
         expect(result).to be_a_kind_of described_class
-        expect(result.path).to eq TEST_JP2_OUTPUT_FILE
-        expect(TEST_JP2_OUTPUT_FILE).to be_a_jp2
+        expect(result.path).to eq output_file
+        expect(output_file).to be_a_jp2
         expect(result.exif.colorspace).to eq 'sRGB'
       end
     end
